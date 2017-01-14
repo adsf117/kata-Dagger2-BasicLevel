6bf40b4f3619988d9f59b60149067233f6f47212
Andres David <ads.f117@gmail.com>
2017-01-13 22:04:43 -0500

    Initial commit

---
+++ app/src/main/java/com/puzzlebench/katadagger2lv1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.puzzlebench.katadagger2lv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
